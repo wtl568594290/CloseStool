cd9b4e27140ea395865ec4a62236d09919f40daa
wtl <568594290@qq.com>
2019-10-15 11:01:59 +0800

    2019.10.15.11.01

--- init.lua
+++ init.lua
@@ -1,12 +1,14 @@
 --io initialize:5 work,6 quantity,7 key,8 config ttl
 workPin = 5
 gpio.write(workPin, gpio.LOW)
+gpio.mode(workPin, gpio.INT)
 gpio.mode(workPin, gpio.INPUT)
 
 gpio.mode(6, gpio.INPUT)
 
 keyPin = 7
 gpio.write(keyPin, gpio.LOW)
+gpio.mode(keyPin, gpio.INT)
 gpio.mode(keyPin, gpio.INPUT)
 
 configPin = 8
